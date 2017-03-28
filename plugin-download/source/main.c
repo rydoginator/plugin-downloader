ad04af8c00f4be51d723688fa9802319a8f4c4e3
Ryan Plante <rplante565@gmail.com>
2017-03-27 22:04:56 -0400

    multi downloader screen, using top screen to print text.

--- plugin-download/source/main.c
+++ plugin-download/source/main.c
@@ -15,17 +15,17 @@ Result http_download(const char *url)
 	u32 contentsize=0, readsize=0, size=0;
 	u8 *buf, *lastbuf;
 
-	printf("Downloading %s\n",url);
+	printf("Downloading your plugin...\n");
 	gfxFlushBuffers();
 
 	do {
 		ret = httpcOpenContext(&context, HTTPC_METHOD_GET, url, 1);
-		printf("return from httpcOpenContext: %"PRId32"\n",ret);
+		//printf("return from httpcOpenContext: %"PRId32"\n",ret);
 		gfxFlushBuffers();
 
 		// This disables SSL cert verification, so https:// will be usable
 		ret = httpcSetSSLOpt(&context, SSLCOPT_DisableVerify);
-		printf("return from httpcSetSSLOpt: %"PRId32"\n",ret);
+		//printf("return from httpcSetSSLOpt: %"PRId32"\n",ret);
 		gfxFlushBuffers();
 
 		// Enable Keep-Alive connections (on by default, pending ctrulib merge)
@@ -35,14 +35,14 @@ Result http_download(const char *url)
 
 		// Set a User-Agent header so websites can identify your application
 		ret = httpcAddRequestHeaderField(&context, "User-Agent", "httpc-example/1.0.0");
-		printf("return from httpcAddRequestHeaderField: %"PRId32"\n",ret);
+		//printf("return from httpcAddRequestHeaderField: %"PRId32"\n",ret);
 		gfxFlushBuffers();
 
 		// Tell the server we can support Keep-Alive connections.
 		// This will delay connection teardown momentarily (typically 5s)
 		// in case there is another request made to the same server.
 		ret = httpcAddRequestHeaderField(&context, "Connection", "Keep-Alive");
-		printf("return from httpcAddRequestHeaderField: %"PRId32"\n",ret);
+		//printf("return from httpcAddRequestHeaderField: %"PRId32"\n",ret);
 		gfxFlushBuffers();
 
 		ret = httpcBeginRequest(&context);
@@ -67,13 +67,13 @@ Result http_download(const char *url)
 			}
 			ret = httpcGetResponseHeader(&context, "Location", newurl, 0x1000);
 			url = newurl; // Change pointer to the url that we just learned
-			printf("redirecting to url: %s\n",url);
+			//printf("redirecting to url: %s\n",url);
 			httpcCloseContext(&context); // Close this context before we try the next
 		}
 	} while ((statuscode >= 301 && statuscode <= 303) || (statuscode >= 307 && statuscode <= 308));
 
 	if(statuscode!=200){
-		printf("URL returned status: %"PRId32"\n", statuscode);
+		//printf("URL returned status: %"PRId32"\n", statuscode);
 		httpcCloseContext(&context);
 		if(newurl!=NULL) free(newurl);
 		return -2;
@@ -87,7 +87,7 @@ Result http_download(const char *url)
 		return ret;
 	}
 
-	printf("reported size: %"PRId32"\n",contentsize);
+	//printf("reported size: %"PRId32"\n",contentsize);
 	gfxFlushBuffers();
 
 	// Start with a single page buffer
@@ -131,21 +131,33 @@ Result http_download(const char *url)
 		return -1;
 	}
 
-	printf("downloaded size: %"PRId32"\n",size);
+	//printf("downloaded size: %"PRId32"\n",size);
 	gfxFlushBuffers();
 
-	FILE *fp;
+	FILE *usa;
+	FILE *eur;
+	FILE *jap;
+
+	//delete any existing plugins in the USA, EUR or JAP directory
+	remove("sdmc:/plugin/0004000000086300/ACNL_Multi.plg");
+	remove("sdmc:/plugin/0004000000086200/ACNL_Multi.plg");
+	remove("sdmc:/plugin/0004000000086400/ACNL_Multi.plg");
+	remove("sdmc:/plugin/0004000000086300/ACNL_Multi_USA.plg");
+	remove("sdmc:/plugin/0004000000086200/ACNL_Multi_JAP.plg");
+	remove("sdmc:/plugin/0004000000086400/ACNL_Multi_EUR.plg");
+	usa = fopen("sdmc:/plugin/0004000000086300/ACNL_MULTI.plg", "w+");
+	fwrite(buf , 1 , size , usa );
+	fclose(usa);
+	eur = fopen("sdmc:/plugin/0004000000086400/ACNL_MULTI.plg", "w+");
+	fwrite(buf , 1 , size , eur );
+	fclose(eur);
+	jap = fopen("sdmc:/plugin/0004000000086200/ACNL_MULTI.plg", "w+");
+	fwrite(buf , 1 , size , jap );
+	fclose(jap);
+
+	printf("Done!\n");
 
 
-	fp = fopen("sdmc:/plugin/0004000000086300/ACNL_Multi.plg", "w+");
-	/*if(fp==NULL)
-	{
-		printf("Failed to open the plugin!");
-		return -3;
-	}*/
-
-	fwrite(buf , 1 , size , fp );
-	fclose(fp);
 
 
 	return 0;
@@ -157,23 +169,31 @@ int main()
 	gfxInitDefault();
 	httpcInit(0); // Buffer size when POST/PUT.
 
-	consoleInit(GFX_BOTTOM,NULL);
+	consoleInit(GFX_TOP,NULL);
 
-	ret=http_download("https://github.com/RyDog199/ACNL-NTR-Cheats/blob/master/ACNL_MULTI.plg?raw=true");
-	// Try the following for redirection to the above URL.
-	// ret=http_download("http://tinyurl.com/hd8jwqx");
-	printf("return from http_download: %"PRId32"\n",ret);
+	printf("---ACNL Multi NTR Plugin Downloader V1.0---\n");
+	printf("Press A to download the latest version \n");
+	printf("Press B to download 3.0 beta (for people without\n the Amiibo update) \n");
+
+
 	gfxFlushBuffers();
 
-	// Main loop
+
 	while (aptMainLoop())
 	{
 		gspWaitForVBlank();
 		hidScanInput();
 
-		// Your code goes here
-
 		u32 kDown = hidKeysDown();
+
+		if (kDown == KEY_A)
+		{
+			ret=http_download("https://github.com/RyDog199/ACNL-NTR-Cheats/blob/master/ACNL_MULTI.plg?raw=true");
+		}
+		if (kDown == KEY_B)
+		{
+			ret=http_download("https://github.com/RyDog199/ACNL-NTR-Cheats/releases/download/v3.0B1/ACNL_MULTI.plg");
+		}
 		if (kDown & KEY_START)
 			break; // break in order to return to hbmenu
 
